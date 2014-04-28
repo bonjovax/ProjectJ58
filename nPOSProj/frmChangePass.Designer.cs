600a2af81152385c8238283663b4fe54f0d14992
scd892 <jarmonilla892@gmail.com>
2014-04-28 19:57:01 +0800

    Added new form
    frmChangePass design

---
+++ nPOSProj/frmChangePass.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace nPOSProj
+{
+    partial class frmChangePass
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_change = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.text_newpassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.text_confirmpassword = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_change
+            // 
+            this.btn_change.Location = new System.Drawing.Point(255, 85);
+            this.btn_change.Name = "btn_change";
+            this.btn_change.Size = new System.Drawing.Size(75, 36);
+            this.btn_change.TabIndex = 0;
+            this.btn_change.Text = "&Change";
+            this.btn_change.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "New Password";
+            // 
+            // text_newpassword
+            // 
+            this.text_newpassword.Location = new System.Drawing.Point(135, 30);
+            this.text_newpassword.Name = "text_newpassword";
+            this.text_newpassword.Size = new System.Drawing.Size(195, 23);
+            this.text_newpassword.TabIndex = 2;
+            this.text_newpassword.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Confirm Password";
+            // 
+            // text_confirmpassword
+            // 
+            this.text_confirmpassword.Location = new System.Drawing.Point(135, 56);
+            this.text_confirmpassword.Name = "text_confirmpassword";
+            this.text_confirmpassword.Size = new System.Drawing.Size(195, 23);
+            this.text_confirmpassword.TabIndex = 2;
+            this.text_confirmpassword.UseSystemPasswordChar = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btn_change);
+            this.groupBox1.Controls.Add(this.text_confirmpassword);
+            this.groupBox1.Controls.Add(this.text_newpassword);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(341, 126);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Change Password Information";
+            // 
+            // frmChangePass
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 150);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmChangePass";
+            this.Text = "Change Password";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_change;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox text_newpassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox text_confirmpassword;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
