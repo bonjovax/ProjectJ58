d319473398f34931de3cf870da5b0458ffebb3cd
Alfel Benvic Go <dlg.alfel@gmail.com>
2014-04-28 13:19:30 +0800

    Added Crypto.cs and dbs.cs
    Added Cryptographic SHA512 Algorithm plus added Dbs.cs Class for the
    Class to app.config.

---
+++ nPOSProj/Conf/Crypto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace nPOSProj.Conf
+{
+    class Crypto
+    {
+        private String getHash;
+
+        //Send
+        public String Hashed(String encode)
+        {
+            try
+            {
+                SHA512 hash = SHA512.Create();
+                ASCIIEncoding encoder = new ASCIIEncoding();
+                byte[] combined = encoder.GetBytes(encode);
+                hash.ComputeHash(combined);
+                getHash = Convert.ToBase64String(hash.Hash);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NOT PRESENT!");
+            }
+            return getHash;
+        }
+
+        //Return
+        public String retreiveHash()
+        {
+            return getHash;
+        }
+    }
+}
---
+++ nPOSProj/Conf/dbs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace nPOSProj.Conf
+{
+    class dbs
+    {
+        private String connectionString;
+
+        public String getConnectionString()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["nPOSProj.Properties.Settings.npos_dbConnectionString"].ConnectionString;
+            return connectionString;
+        }
+    }
+}
