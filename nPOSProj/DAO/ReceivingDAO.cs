091baba9d67d83c810fc7b64285d4295c33e4f9d
Alfel Benvic Go <dlg.alfel@gmail.com>
2014-05-17 16:31:13 +0800

    Receiving Form
    Partial Coding Implementation

--- nPOSProj/DAO/ReceivingDAO.cs
+++ nPOSProj/DAO/ReceivingDAO.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 using System.Linq;
 using System.Text;
 
@@ -7,5 +10,31 @@ namespace nPOSProj.DAO
 {
     class ReceivingDAO
     {
+        private MySqlConnection con;
+        private Conf.dbs dbcon;
+
+        public ReceivingDAO()
+        {
+
+        }
+
+        public void Receive(Int32 po_no, String order_suppliers_itemno, Int32 quantity)
+        {
+            con = new MySqlConnection();
+            dbcon = new Conf.dbs();
+            con.ConnectionString = dbcon.getConnectionString();
+            String query = "UPDATE po_order_list SET order_quantity = order_quantity - ?order_quantity ";
+            query += "WHERE po_no = ?po_no AND order_suppliers_itemno = ?order_suppliers_itemno";
+            String query1 = "UPDATE inventory_stocks SET stock_quantity = stock_quantity + ?stock_quantity, stock_total_price = ?stock_total_price ";
+            query1 += "WHERE stock_code = ?stock_code";
+            try
+            {
+                con.Open();
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
     }
 }
