ff7673dbb9dc8d086290f1bff052448be976b24f
scd892 <jarmonilla892@gmail.com>
2014-05-28 11:44:04 +0800

    Design
    modification on design

--- nPOSProj/frmPOS.Designer.cs
+++ nPOSProj/frmPOS.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem(new string[] {
             "1234567890123",
             "222",
             "Palmolive Shampoo",
@@ -59,6 +59,10 @@
             this.button3 = new System.Windows.Forms.Button();
             this.textBox9 = new System.Windows.Forms.TextBox();
             this.button4 = new System.Windows.Forms.Button();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // listView1
@@ -75,11 +79,11 @@
             this.listView1.FullRowSelect = true;
             this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
             this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem1});
-            this.listView1.Location = new System.Drawing.Point(262, 94);
+            listViewItem2});
+            this.listView1.Location = new System.Drawing.Point(297, 118);
             this.listView1.MultiSelect = false;
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(827, 515);
+            this.listView1.Size = new System.Drawing.Size(823, 510);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -121,44 +125,44 @@
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.Color.Black;
-            this.textBox1.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Font = new System.Drawing.Font("Courier New", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.ForeColor = System.Drawing.Color.Lime;
             this.textBox1.Location = new System.Drawing.Point(-2, -2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(1091, 49);
+            this.textBox1.Size = new System.Drawing.Size(1212, 62);
             this.textBox1.TabIndex = 1;
             this.textBox1.Text = "Description";
             // 
             // textBox2
             // 
             this.textBox2.BackColor = System.Drawing.Color.MidnightBlue;
-            this.textBox2.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Font = new System.Drawing.Font("Courier New", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox2.ForeColor = System.Drawing.Color.Lime;
-            this.textBox2.Location = new System.Drawing.Point(-2, 45);
+            this.textBox2.Location = new System.Drawing.Point(-2, 58);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(145, 49);
+            this.textBox2.Size = new System.Drawing.Size(145, 62);
             this.textBox2.TabIndex = 2;
             this.textBox2.Text = "Qty";
             // 
             // textBox3
             // 
             this.textBox3.BackColor = System.Drawing.Color.MidnightBlue;
-            this.textBox3.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Font = new System.Drawing.Font("Courier New", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox3.ForeColor = System.Drawing.Color.Lime;
-            this.textBox3.Location = new System.Drawing.Point(185, 45);
+            this.textBox3.Location = new System.Drawing.Point(193, 58);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(329, 49);
+            this.textBox3.Size = new System.Drawing.Size(329, 62);
             this.textBox3.TabIndex = 3;
             this.textBox3.Text = "P9999";
             // 
             // textBox4
             // 
             this.textBox4.BackColor = System.Drawing.Color.MidnightBlue;
-            this.textBox4.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Font = new System.Drawing.Font("Courier New", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox4.ForeColor = System.Drawing.Color.Lime;
-            this.textBox4.Location = new System.Drawing.Point(512, 45);
+            this.textBox4.Location = new System.Drawing.Point(520, 58);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(577, 49);
+            this.textBox4.Size = new System.Drawing.Size(690, 62);
             this.textBox4.TabIndex = 4;
             this.textBox4.Text = "Total";
             this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
@@ -167,23 +171,23 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.MidnightBlue;
-            this.label1.Font = new System.Drawing.Font("Courier New", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Courier New", 38.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Transparent;
-            this.label1.Location = new System.Drawing.Point(142, 47);
+            this.label1.Location = new System.Drawing.Point(141, 61);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 46);
+            this.label1.Size = new System.Drawing.Size(56, 57);
             this.label1.TabIndex = 5;
             this.label1.Text = "@";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // textBox5
             // 
-            this.textBox5.BackColor = System.Drawing.Color.Lime;
+            this.textBox5.BackColor = System.Drawing.Color.Gold;
             this.textBox5.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox5.ForeColor = System.Drawing.Color.Red;
-            this.textBox5.Location = new System.Drawing.Point(-1, 560);
+            this.textBox5.Location = new System.Drawing.Point(-1, 498);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(263, 49);
+            this.textBox5.Size = new System.Drawing.Size(298, 49);
             this.textBox5.TabIndex = 6;
             this.textBox5.Text = "P99999.00";
             this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
@@ -194,7 +198,7 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Courier New", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.MidnightBlue;
-            this.label2.Location = new System.Drawing.Point(4, 332);
+            this.label2.Location = new System.Drawing.Point(4, 270);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(167, 36);
             this.label2.TabIndex = 7;
@@ -206,9 +210,9 @@
             this.textBox6.BackColor = System.Drawing.Color.Black;
             this.textBox6.Font = new System.Drawing.Font("Courier New", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox6.ForeColor = System.Drawing.Color.Lime;
-            this.textBox6.Location = new System.Drawing.Point(104, 370);
+            this.textBox6.Location = new System.Drawing.Point(68, 305);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(158, 47);
+            this.textBox6.Size = new System.Drawing.Size(229, 47);
             this.textBox6.TabIndex = 8;
             this.textBox6.Text = "P9999";
             this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -218,9 +222,9 @@
             this.textBox7.BackColor = System.Drawing.Color.Black;
             this.textBox7.Font = new System.Drawing.Font("Courier New", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox7.ForeColor = System.Drawing.Color.Lime;
-            this.textBox7.Location = new System.Drawing.Point(105, 449);
+            this.textBox7.Location = new System.Drawing.Point(68, 387);
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(157, 47);
+            this.textBox7.Size = new System.Drawing.Size(229, 47);
             this.textBox7.TabIndex = 10;
             this.textBox7.Text = "0%";
             this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
@@ -231,7 +235,7 @@
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Courier New", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.MidnightBlue;
-            this.label3.Location = new System.Drawing.Point(6, 417);
+            this.label3.Location = new System.Drawing.Point(6, 355);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(72, 36);
             this.label3.TabIndex = 9;
@@ -242,11 +246,11 @@
             // 
             this.textBox8.BackColor = System.Drawing.Color.MidnightBlue;
             this.textBox8.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox8.ForeColor = System.Drawing.Color.Lime;
-            this.textBox8.Location = new System.Drawing.Point(-1, 511);
+            this.textBox8.ForeColor = System.Drawing.Color.White;
+            this.textBox8.Location = new System.Drawing.Point(-1, 449);
             this.textBox8.Name = "textBox8";
             this.textBox8.ReadOnly = true;
-            this.textBox8.Size = new System.Drawing.Size(264, 49);
+            this.textBox8.Size = new System.Drawing.Size(298, 49);
             this.textBox8.TabIndex = 11;
             this.textBox8.Text = "TOTAL";
             this.textBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -255,9 +259,9 @@
             // 
             this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.button1.Location = new System.Drawing.Point(262, 615);
+            this.button1.Location = new System.Drawing.Point(386, 629);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(119, 104);
+            this.button1.Size = new System.Drawing.Size(119, 106);
             this.button1.TabIndex = 12;
             this.button1.UseVisualStyleBackColor = true;
             // 
@@ -265,9 +269,9 @@
             // 
             this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
             this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.button2.Location = new System.Drawing.Point(387, 615);
+            this.button2.Location = new System.Drawing.Point(511, 629);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(119, 104);
+            this.button2.Size = new System.Drawing.Size(119, 106);
             this.button2.TabIndex = 13;
             this.button2.UseVisualStyleBackColor = true;
             // 
@@ -275,9 +279,9 @@
             // 
             this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
             this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.button3.Location = new System.Drawing.Point(512, 615);
+            this.button3.Location = new System.Drawing.Point(636, 629);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(119, 104);
+            this.button3.Size = new System.Drawing.Size(119, 106);
             this.button3.TabIndex = 14;
             this.button3.UseVisualStyleBackColor = true;
             // 
@@ -286,9 +290,9 @@
             this.textBox9.BackColor = System.Drawing.Color.MidnightBlue;
             this.textBox9.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox9.ForeColor = System.Drawing.Color.Lime;
-            this.textBox9.Location = new System.Drawing.Point(-1, 192);
+            this.textBox9.Location = new System.Drawing.Point(-1, 206);
             this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(264, 49);
+            this.textBox9.Size = new System.Drawing.Size(298, 49);
             this.textBox9.TabIndex = 15;
             this.textBox9.Text = "EAN";
             // 
@@ -296,11 +300,61 @@
             // 
             this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
             this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.button4.Location = new System.Drawing.Point(-1, 94);
+            this.button4.Location = new System.Drawing.Point(0, 118);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(263, 99);
+            this.button4.Size = new System.Drawing.Size(297, 91);
             this.button4.TabIndex = 16;
             this.button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox10
+            // 
+            this.textBox10.BackColor = System.Drawing.Color.MidnightBlue;
+            this.textBox10.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox10.ForeColor = System.Drawing.Color.White;
+            this.textBox10.Location = new System.Drawing.Point(-1, 628);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.ReadOnly = true;
+            this.textBox10.Size = new System.Drawing.Size(298, 49);
+            this.textBox10.TabIndex = 18;
+            this.textBox10.Text = "Change";
+            this.textBox10.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox11
+            // 
+            this.textBox11.BackColor = System.Drawing.Color.Gold;
+            this.textBox11.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox11.ForeColor = System.Drawing.Color.Red;
+            this.textBox11.Location = new System.Drawing.Point(0, 679);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(297, 49);
+            this.textBox11.TabIndex = 17;
+            this.textBox11.Text = "P1.00";
+            this.textBox11.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox12
+            // 
+            this.textBox12.BackColor = System.Drawing.Color.Black;
+            this.textBox12.Font = new System.Drawing.Font("Courier New", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox12.ForeColor = System.Drawing.Color.Lime;
+            this.textBox12.Location = new System.Drawing.Point(68, 578);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(229, 47);
+            this.textBox12.TabIndex = 20;
+            this.textBox12.Text = "P9999";
+            this.textBox12.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Courier New", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label4.Location = new System.Drawing.Point(4, 549);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 36);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Tender";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // frmPOS
             // 
@@ -308,6 +362,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1204, 731);
             this.ControlBox = false;
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox11);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.textBox9);
             this.Controls.Add(this.button3);
@@ -362,5 +420,9 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox9;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.Label label4;
     }
 }
