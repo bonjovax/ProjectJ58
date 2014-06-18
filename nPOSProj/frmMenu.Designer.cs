1a49f97a226cb6e4e8cfaf1fda1a12acacc4f742
Alfel Benvic Go <dlg.alfel@gmail.com>
2014-06-18 16:20:56 +0800

    Frm Menu
    Edges

--- nPOSProj/frmMenu.Designer.cs
+++ nPOSProj/frmMenu.Designer.cs
@@ -56,7 +56,7 @@
             this.pictureBox1.BackColor = System.Drawing.Color.WhiteSmoke;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(700, 115);
+            this.pictureBox1.Size = new System.Drawing.Size(701, 115);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
