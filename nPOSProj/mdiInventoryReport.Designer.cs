67afac13796130817f0201c1bb9e344e25af4e8f
Alfel Benvic Go <dlg.alfel@gmail.com>
2014-07-02 19:17:01 +0800

    Inventory Reports
    Removed Left Overs!
    Please Test!

--- nPOSProj/mdiInventoryReport.Designer.cs
+++ nPOSProj/mdiInventoryReport.Designer.cs
@@ -30,9 +30,9 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mdiInventoryReport));
             this.tabPage4 = new System.Windows.Forms.TabPage();
-            this.btnItem = new System.Windows.Forms.Button();
-            this.btnKits = new System.Windows.Forms.Button();
             this.btnStocks = new System.Windows.Forms.Button();
+            this.btnKits = new System.Windows.Forms.Button();
+            this.btnItem = new System.Windows.Forms.Button();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.btnPrintSR = new System.Windows.Forms.Button();
             this.tabControl1 = new System.Windows.Forms.TabControl();
@@ -54,26 +54,26 @@
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "Summary";
             // 
-            // btnItem
+            // btnStocks
             // 
-            this.btnItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(126)))), ((int)(((byte)(56)))), ((int)(((byte)(120)))));
-            this.btnItem.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnItem.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(83)))), ((int)(((byte)(44)))));
-            this.btnItem.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
-            this.btnItem.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(83)))), ((int)(((byte)(44)))));
-            this.btnItem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnItem.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnItem.ForeColor = System.Drawing.Color.White;
-            this.btnItem.Image = ((System.Drawing.Image)(resources.GetObject("btnItem.Image")));
-            this.btnItem.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnItem.Location = new System.Drawing.Point(224, 90);
-            this.btnItem.Name = "btnItem";
-            this.btnItem.Size = new System.Drawing.Size(126, 69);
-            this.btnItem.TabIndex = 10;
-            this.btnItem.Text = "Item";
-            this.btnItem.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnItem.UseVisualStyleBackColor = false;
-            this.btnItem.Click += new System.EventHandler(this.btnItem_Click);
+            this.btnStocks.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(126)))), ((int)(((byte)(56)))), ((int)(((byte)(120)))));
+            this.btnStocks.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStocks.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(83)))), ((int)(((byte)(44)))));
+            this.btnStocks.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.btnStocks.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(83)))), ((int)(((byte)(44)))));
+            this.btnStocks.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStocks.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStocks.ForeColor = System.Drawing.Color.White;
+            this.btnStocks.Image = ((System.Drawing.Image)(resources.GetObject("btnStocks.Image")));
+            this.btnStocks.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnStocks.Location = new System.Drawing.Point(285, 165);
+            this.btnStocks.Name = "btnStocks";
+            this.btnStocks.Size = new System.Drawing.Size(135, 69);
+            this.btnStocks.TabIndex = 12;
+            this.btnStocks.Text = "Stocks";
+            this.btnStocks.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnStocks.UseVisualStyleBackColor = false;
+            this.btnStocks.Click += new System.EventHandler(this.btnStocks_Click);
             // 
             // btnKits
             // 
@@ -96,26 +96,26 @@
             this.btnKits.UseVisualStyleBackColor = false;
             this.btnKits.Click += new System.EventHandler(this.btnKits_Click);
             // 
-            // btnStocks
+            // btnItem
             // 
-            this.btnStocks.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(126)))), ((int)(((byte)(56)))), ((int)(((byte)(120)))));
-            this.btnStocks.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnStocks.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(83)))), ((int)(((byte)(44)))));
-            this.btnStocks.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
-            this.btnStocks.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(83)))), ((int)(((byte)(44)))));
-            this.btnStocks.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnStocks.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnStocks.ForeColor = System.Drawing.Color.White;
-            this.btnStocks.Image = ((System.Drawing.Image)(resources.GetObject("btnStocks.Image")));
-            this.btnStocks.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnStocks.Location = new System.Drawing.Point(285, 165);
-            this.btnStocks.Name = "btnStocks";
-            this.btnStocks.Size = new System.Drawing.Size(135, 69);
-            this.btnStocks.TabIndex = 12;
-            this.btnStocks.Text = "Stocks";
-            this.btnStocks.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnStocks.UseVisualStyleBackColor = false;
-            this.btnStocks.Click += new System.EventHandler(this.btnStocks_Click);
+            this.btnItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(126)))), ((int)(((byte)(56)))), ((int)(((byte)(120)))));
+            this.btnItem.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnItem.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(83)))), ((int)(((byte)(44)))));
+            this.btnItem.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.btnItem.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(83)))), ((int)(((byte)(44)))));
+            this.btnItem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnItem.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnItem.ForeColor = System.Drawing.Color.White;
+            this.btnItem.Image = ((System.Drawing.Image)(resources.GetObject("btnItem.Image")));
+            this.btnItem.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnItem.Location = new System.Drawing.Point(224, 90);
+            this.btnItem.Name = "btnItem";
+            this.btnItem.Size = new System.Drawing.Size(126, 69);
+            this.btnItem.TabIndex = 10;
+            this.btnItem.Text = "Item";
+            this.btnItem.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnItem.UseVisualStyleBackColor = false;
+            this.btnItem.Click += new System.EventHandler(this.btnItem_Click);
             // 
             // tabPage2
             // 
@@ -172,7 +172,6 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "mdiInventoryReport";
             this.Text = "Inventory Report";
-            this.Load += new System.EventHandler(this.mdiInventoryReport_Load);
             this.tabPage4.ResumeLayout(false);
             this.tabPage2.ResumeLayout(false);
             this.tabControl1.ResumeLayout(false);
--- nPOSProj/mdiInventoryReport.cs
+++ nPOSProj/mdiInventoryReport.cs
@@ -12,83 +12,9 @@ namespace nPOSProj
 {
     public partial class mdiInventoryReport : Form
     {
-        private MySqlConnection con = new MySqlConnection();
-        private Conf.dbs dbcon = new Conf.dbs();
-        private DAO.LoginDAO login;
-        private Conf.BIR bir = new Conf.BIR(); //Bureau of Internal Revenue - PH
-        private VO.ReportingVO reports = new VO.ReportingVO();
-        private VO.ConfigVO config;
-        private String userName = frmLogin.User.user_name;
-        private String terminalIR;
-
         public mdiInventoryReport()
         {
             InitializeComponent();
-        }
-
-        private void checkRes()
-        {
-            login = new DAO.LoginDAO();
-            String userName = frmLogin.User.user_name;
-            login.catchUsername(userName);
-            if (login.hasUser_Accounts())
-            {
-                //lblX.Visible = true;
-                //cBTerminalIR.Visible = true;
-            }
-        }
-
-        private void getTerminal()
-        {
-            try
-            {
-                config = new VO.ConfigVO();
-                //cBTerminalIR.Items.Clear();
-                String[,] grabData = config.ReadTerminal();
-                for (int x = 0; x < grabData.GetLength(1); x++)
-                {
-                    //cBTerminalIR.Items.Add(grabData[0, x].ToString());
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString());
-            }
-        }
-
-        private void mdiInventoryReport_Load(object sender, EventArgs e)
-        {
-            frmLogin fl = new frmLogin();
-            terminalIR = fl.tN;
-            getTerminal();
-            checkRes();
-        }
-
-        //private void btnPrint_Click(object sender, EventArgs e)
-        //{
-        //    if (cBoxKitSelect.Checked == true)
-        //    {
-        //        using (frmRptISRkit isrkit = new frmRptISRkit())
-        //        {
-        //            isrkit.DateParam = dtSIR.Text;
-        //            isrkit.TerminalParam = terminalIR;
-        //            isrkit.ShowDialog();
-        //        }
-        //    }
-        //    else
-        //    {
-        //        using (frmRptISR isr = new frmRptISR())
-        //        {
-        //            isr.DateParam = dtSIR.Text;
-        //            isr.TerminalParam = terminalIR;
-        //            isr.ShowDialog();
-        //        }
-        //    }
-        //}
-
-        private void cBTerminalIR_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            //terminalIR = cBTerminalIR.Text;
         }
 
         private void btnPrintSR_Click(object sender, EventArgs e)
