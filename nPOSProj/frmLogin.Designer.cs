b4ca976c36c70bf914b06b11030dbfc58ef5181a
Alfel Benvic Go <dlg.alfel@gmail.com>
2014-07-03 16:05:13 +0800

    Application Header
    Images Added!

--- nPOSProj/frmLogin.Designer.cs
+++ nPOSProj/frmLogin.Designer.cs
@@ -49,10 +49,10 @@
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.LightGray;
-            this.pictureBox1.Location = new System.Drawing.Point(-2, 0);
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(418, 97);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.Size = new System.Drawing.Size(413, 97);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
